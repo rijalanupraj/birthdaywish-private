696c1370bc816f1adcaa69246e2b6f493b6be845
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-08-02 22:49:44 +0545

    Dispaly All birthday of today-backend/frontend

--- friend/models.py
+++ friend/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Friend(models.Model):
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    short_intro = models.TextField(max_length=255)
+    dob = models.DateField()
+    description = models.TextField(null=True, blank=True)
+    picture1 = models.ImageField(upload_to="images/profile_pic")
+    picture2 = models.ImageField(
+        blank=True, null=True, upload_to="images/profile_pic")
+    picture3 = models.ImageField(
+        blank=True, null=True, upload_to="images/profile_pic")
+    facebook_handle = models.CharField(max_length=255, blank=True, null=True)
+    twitter_handle = models.CharField(max_length=255, blank=True, null=True)
+    github_handle = models.CharField(max_length=255, blank=True, null=True)
+    instagram_handle = models.CharField(max_length=255, blank=True, null=True)
+    youtube_handle = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
--- friend/views.py
+++ friend/views.py
@@ -1,7 +1,18 @@
+# External Import
 from django.shortcuts import render
+import datetime
 
-# Create your views here.
+
+# Internal Import
+from friend.models import Friend
 
 
 def homerender(request):
-    return render(request, 'friend/home-page.html')
+    today_date = datetime.datetime.now().date()
+    today_birthdays = Friend.objects.filter(
+        dob=today_date, is_active=True)
+    print(today_birthdays)
+    context = {
+        'birthdays': today_birthdays,
+    }
+    return render(request, 'friend/home-page.html', context)
