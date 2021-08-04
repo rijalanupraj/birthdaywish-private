b2e7fdc852ef77342e0b7c8d1d594e326531242e
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-08-04 15:03:18 +0545

    Add Procfile and allowed hosts

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn birthdaywishprivate.wsgi
--- birthdaywishprivate/settings.py
+++ birthdaywishprivate/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nawa-birthday-wisher.herokuapp.com']
 
 
 # Application definition
